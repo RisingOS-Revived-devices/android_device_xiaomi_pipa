db41a5b813142972ac306f7ca517da7200a865ec
Aryan Biswas <152459091+CuriousNom@users.noreply.github.com>
2025-02-07 08:12:35 +0530

    pipa: Update org links

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
-git clone --depth 1 https://github.com/CuriousNom/android_device_xiaomi_sm8250-common device/xiaomi/sm8250-common
-git clone --depth 1 https://github.com/CuriousNom/proprietary_vendor_xiaomi_pipa vendor/xiaomi/pipa 
-git clone --depth 1 https://github.com/TheMuppets/proprietary_vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common
-git clone --depth 1 https://github.com/CuriousNom/android_kernel_xiaomi_sm8250 kernel/xiaomi/sm8250
+git clone --depth 1 https://github.com/Matrixx-Devices/android_device_xiaomi_sm8250-common device/xiaomi/sm8250-common
+git clone --depth 1 https://github.com/Matrixx-Devices/proprietary_vendor_xiaomi_pipa vendor/xiaomi/pipa 
+git clone --depth 1 https://github.com/Matrixx-Devices/proprietary_vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common
+git clone --depth 1 https://github.com/Matrixx-Devices/android_kernel_xiaomi_pipa kernel/xiaomi/sm8250
 git clone --depth 1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 git clone --depth 1 https://github.com/PixelOS-Devices/vendor_xiaomi_pipa-miuicamera vendor/xiaomi/pipa-miuicamera
