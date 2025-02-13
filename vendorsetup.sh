e43b7a18b79b3f325f2ba3fc7c0e81e66321dccd
Aryan Biswas <152459091+CuriousNom@users.noreply.github.com>
2025-02-13 23:12:25 +0530

    pipa: track xiaomi hardware stuff from crdroid

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,5 +2,5 @@ git clone --depth 1 https://github.com/Matrixx-Devices/android_device_xiaomi_sm8
 git clone --depth 1 https://github.com/Matrixx-Devices/proprietary_vendor_xiaomi_pipa vendor/xiaomi/pipa 
 git clone --depth 1 https://github.com/Matrixx-Devices/proprietary_vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common
 git clone --depth 1 https://github.com/Matrixx-Devices/android_kernel_xiaomi_pipa kernel/xiaomi/sm8250
-git clone --depth 1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
+git clone --depth 1 https://github.com/crdroidandroid/android_hardware_xiaomi.git hardware/xiaomi
 git clone --depth 1 https://github.com/PixelOS-Devices/vendor_xiaomi_pipa-miuicamera vendor/xiaomi/pipa-miuicamera
