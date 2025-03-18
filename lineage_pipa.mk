d2bcbfe6e5652a74e4b5bde9449e543efd462aa7
Aryan Biswas <152459091+CuriousNom@users.noreply.github.com>
2025-03-18 19:16:32 +0530

    pipa: Bringup RisingOS Revived

--- lineage_pipa.mk
+++ lineage_pipa.mk
@@ -9,17 +9,27 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Matrixx
-MATRIXX_BUILD_TYPE := Official
-MATRIXX_MAINTAINER := Aryan
-MATRIXX_CHIPSET := Snapdragon 870
-MATRIXX_BATTERY := 8840mAh
-MATRIXX_DISPLAY := 1800x2880
+# Rising Flags
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="Snapdragon 870" \
+    RisingMaintainer="Aryan"
+
+RISING_MAINTAINER := Aryan
+TARGET_ENABLE_BLUR := true
 WITH_GMS := true
-TARGET_SUPPORTS_WALLEFFECT := true
-BYPASS_CHARGE_SUPPORTED  := true
+TARGET_CORE_GMS := true
+TARGET_CORE_GMS_EXTRAS := true
+PRODUCT_PACKAGES += \
+    Photos \
+    AiWallpapers \
+    LatinIMEGooglePrebuilt \
+    WallpaperEmojiPrebuilt \
+    PrebuiltDeskClockGoogle \
+    CalculatorGooglePrebuilt \
+    CalendarGooglePrebuilt \
+    Velvet
 
 # Inherit from pipa device
 $(call inherit-product, device/xiaomi/pipa/device.mk)
