908d620611d5373832f79c1489fb4d0636cd924e
Aryan Biswas <152459091+CuriousNom@users.noreply.github.com>
2025-04-24 20:30:36 +0530

    pipa: Switch to wifi only builds

--- lineage_pipa.mk
+++ lineage_pipa.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 # Rising Flags
 PRODUCT_BUILD_PROP_OVERRIDES += \
