214fe352626ac86aecf38950d21f7cec26031e6f
CuriousNom <aryanbiswas7585@gmail.com>
2025-05-22 19:18:19 +0530

    pipa: Update props to OS2.0.7.0.UMZCNXM

--- lineage_pipa.mk
+++ lineage_pipa.mk
@@ -36,4 +36,4 @@ TARGET_SUPPORTS_QUICK_TAP := false
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildFingerprint=Xiaomi/pipa_global/pipa:13/RKQ1.211001.001/V816.0.12.0.UMZMIXM:user/release-keys
+    BuildFingerprint=Android/missi_pad_cn/missi:14/UKQ1.240624.001OS2.0.7.0.UMZCNXM:user/release-keys
