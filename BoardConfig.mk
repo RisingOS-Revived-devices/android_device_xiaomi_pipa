c162cb5c56cd4a454104d8ac1ca1cf5f68a9c826
CuriousNom <aryanbiswas7585@gmail.com>
2025-07-28 10:24:01 +0530

    pipa: Only assert pipa
    Change-Id: I2dc6048b0298653a189a9d0caea5d418d638a036

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@ TARGET_SCREEN_DENSITY := 440
 TARGET_KERNEL_CONFIG += vendor/xiaomi/pipa.config
 
 # OTA assert
-TARGET_OTA_ASSERT_DEVICE := pipa,pipain
+TARGET_OTA_ASSERT_DEVICE := pipa
 
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
