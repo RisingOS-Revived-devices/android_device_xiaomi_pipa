e3869ac940d935e9cf2e3149767117eab858ac8f
CuriousNom <aryanbiswas7585@gmail.com>
2025-07-28 10:01:45 +0530

    treewide: alioth -> pipa
    Change-Id: Ie277a31826f877aea9a4756c108ac32412d60646

--- libinit/init_xiaomi_alioth.cpp
+++ libinit/init_xiaomi_alioth.cpp
@@ -9,49 +9,49 @@
 
 #include "vendor_init.h"
 
-static const variant_info_t aliothin_info = {
+static const variant_info_t pipain_info = {
     .hwc_value = "INDIA",
     .sku_value = "",
 
     .brand = "Mi",
-    .device = "aliothin",
+    .device = "pipain",
     .marketname = "Mi 11X",
     .model = "M2012K11AI",
-    .build_fingerprint = "Mi/aliothin/aliothin:13/TKQ1.221114.001/V816.0.2.0.TKHINXM:user/release-keys",
+    .build_fingerprint = "Mi/pipain/pipain:13/TKQ1.221114.001/V816.0.2.0.TKHINXM:user/release-keys",
 
     .nfc = false,
 };
 
-static const variant_info_t alioth_global_info = {
+static const variant_info_t pipa_global_info = {
     .hwc_value = "GLOBAL",
     .sku_value = "",
 
     .brand = "POCO",
-    .device = "alioth",
+    .device = "pipa",
     .marketname = "POCO F3",
     .model = "M2012K11AG",
-    .build_fingerprint = "POCO/alioth_global/alioth:13/TKQ1.221114.001/V816.0.3.0.TKHMIXM:user/release-keys",
+    .build_fingerprint = "POCO/pipa_global/pipa:13/TKQ1.221114.001/V816.0.3.0.TKHMIXM:user/release-keys",
 
     .nfc = true,
 };
 
-static const variant_info_t alioth_info = {
+static const variant_info_t pipa_info = {
     .hwc_value = "",
     .sku_value = "",
 
     .brand = "Redmi",
-    .device = "alioth",
+    .device = "pipa",
     .marketname = "Redmi K40",
     .model = "M2012K11AC",
-    .build_fingerprint = "Redmi/alioth/alioth:13/TKQ1.221114.001/V816.0.6.0.TKHCNXM:user/release-keys",
+    .build_fingerprint = "Redmi/pipa/pipa:13/TKQ1.221114.001/V816.0.6.0.TKHCNXM:user/release-keys",
 
     .nfc = true,
 };
 
 static const std::vector<variant_info_t> variants = {
-    aliothin_info,
-    alioth_global_info,
-    alioth_info,
+    pipain_info,
+    pipa_global_info,
+    pipa_info,
 };
 
 void vendor_load_properties() {
